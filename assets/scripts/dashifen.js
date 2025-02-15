8825ab59d8dedb0beb75c2d74e3ce44249f7cc77
David Dashifen Kees <dashifen@dashifen.com>
2025-02-15 10:57:35 -0500

    better fill for arc

--- assets/scripts/dashifen.js
+++ assets/scripts/dashifen.js
@@ -5,18 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
   if (canvas.getContext) {
     const ctx = canvas.getContext('2d');
     const progress = canvas.dataset.progress;
-    ctx.fillStyle = 'rgba(255,255,255,0.25)';
+    ctx.fillStyle = 'rgba(255,255,255,0.1)';
+    ctx.strokeStyle = 'white';
     
     ctx.beginPath();
     ctx.lineWidth = 5;
-    ctx.strokeStyle = 'white';
     ctx.arc(canvas.width / 2, canvas.height / 2, 70, 0, 2 * Math.PI * progress);
     ctx.stroke();
     ctx.fill();
     
     ctx.beginPath();
     ctx.lineWidth = 1;
-    ctx.strokeStyle = 'gray'
     ctx.arc(canvas.width / 2, canvas.height / 2, 70, 2 * Math.PI * progress, 2 * Math.PI);
     ctx.stroke();
     ctx.fill();
